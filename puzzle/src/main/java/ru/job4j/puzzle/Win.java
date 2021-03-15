38a6645a8709a3bb3b3f7304beece069893d7e5a
kva.devops <kva.devops@gmail.com>
2021-03-15 13:21:15 +0400

    2. Сокобан. Тестовое задание. [#484919] - correcting

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,49 +3,43 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
-        if (checkHorizontal(board) || checkVertical(board)) {
-            rsl = true;
+        int[] diagonalArr = extractDiagonal(board);
+        for (int i = 0; i < board.length; i++) {
+            if (diagonalArr[i] == 1 && (checkHorizontal(board, i) || checkVertical(board, i))) {
+                rsl = true;
+                break;
+            }
         }
         return rsl;
     }
 
-    //check horizontal
-    public static boolean checkHorizontal(int[][] board) {
-        boolean result = false;
+    // extract diagonal
+    public static int[] extractDiagonal(int[][] board) {
+        int[] rsl = new int[board.length];
         for (int i = 0; i < board.length; i++) {
-            int counter = 0;
-            if (board[i][i] == 1) {
-                for (int j = 0; j < board[i].length; j++) {
-                    if (board[i][j] != 1) {
-                        break;
-                    } else {
-                        counter++;
-                    }
-                }
-            }
-            if (counter == board[0].length) {
-               result = true;
+            rsl[i] = board[i][i];
+        }
+        return rsl;
+    }
+    //check horizontal
+    public static boolean checkHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
             }
         }
         return result;
     }
 
     //check vertical
-    public static boolean checkVertical(int[][] board) {
-        boolean result = false;
+    public static boolean checkVertical(int[][] board, int col) {
+        boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            int counter = 0;
-            if (board[i][i] == 1) {
-                for (int j = 0; j < board[i].length; j++) {
-                    if (board[j][i] != 1) {
-                        break;
-                    } else {
-                        counter++;
-                    }
-                }
-            }
-            if (counter == board[0].length) {
-                result = true;
+            if (board[i][col] != 1) {
+                result = false;
+                break;
             }
         }
         return result;
